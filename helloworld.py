c5b9b27b86b07d19463bc3d52f83755fc5938b23
Kyle Brown <umbrow92@myumanitoba.ca>
2014-02-07 11:12:59 -0600

    working db pulling
    DB connection and basic printing of the data pulled

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,16 @@
 from flask import Flask
+import sqlite3
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-	return "Hello worldddddddddd! :)!"
+        conn = sqlite3.connect('/var/www/softeng2/testdb.db')
+        stuff = ""
+        c = conn.cursor()
+        for row in c.execute("select * from testtable"):
+                stuff = stuff + str(row) + "<br>"
+        stuff = stuff + "<br>"
+        return ("Hello worldddddddddd! :)!<br>This is group 7s Soft Eng landing space" + "<br>This is raw data pulled from our db:<br>" + stuff)
 
 if __name__ == "__main__":
-	app.run()
+        app.run(debug=True, host='0.0.0.0', port=int('80'))
