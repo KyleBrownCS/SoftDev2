18c85b68b928e61e2b97eb82c99e34ce50c25794
Dayton1992 <drdayton92@gmail.com>
2014-02-28 15:41:13 -0600

    Implemented support for different statuses
    Default status will be set to none unless selected otherwise.

--- static/schedule.js
+++ static/schedule.js
@@ -1,11 +1,17 @@
 $(document).ready(function(e) { 
     $.get('/obligations', function(data) {
-
     	//Properly format data for exit.
     	var mydata = data.trim().replace(/\(/g, "");
     	mydata = mydata.replace(/\)/g, "");
     	var datas = mydata.split("|");
-
+		//The column for status
+		var statusCol = 7;
+		var statuses = new Array(5);
+		statuses[0] = "none";
+		statuses[1] = "In Progress";
+		statuses[2] = "Completed";
+		statuses[3] = "Important";
+		statuses[4] = "Requires Assistance";
     	var heading = "<table border='1'><th>ObligationID</th><th>UserID</th><th>Name</th><th>Description</th><th>StartTime</th><th>EndTime</th><th>Priority</th><th>Status</th><th>Category</th>";
     	var mainData = "";
     	var currline = "<tr>"
@@ -16,7 +22,15 @@ $(document).ready(function(e) {
     		{
     			miniparts[j] = miniparts[j].replace(/\u'/g, "");
     			miniparts[j] = miniparts[j].replace(/\'/g, "");
-    			currline = currline + "<td>" + miniparts[j] + "</td>";
+			//If it's processing a status, show the string instead of the integer value
+			if(j == statusCol)
+			{
+				currline = currline + "<td>" + statuses[miniparts[j]] + "</td>";
+			}
+			else
+			{
+				currline = currline + "<td>" + miniparts[j] + "</td>";
+			}
     		}
     		heading = heading + currline + "</tr>";
     		currline = "<tr>";
