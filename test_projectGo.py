3692e6b1dacded4b7be23ca3a3218ad188f4667e
Dayton1992 <drdayton92@gmail.com>
2014-02-27 23:25:07 -0600

    Added various test cases
    Modified the test file to give different cases

--- test_projectGo.py
+++ test_projectGo.py
@@ -20,25 +20,60 @@ class Test(unittest.TestCase):
         
 	#test of direct method for get_obligation
 	def test_go_get_obligation_exists(self):
-		result = projectGo.get_obligation(1)
-		jsonData = json.loads(result)
+		#Edge case low
+		jsonData = json.loads(projectGo.get_obligation(1))
 		self.assertEqual(jsonData['obligationid'],1)
+		#Edge case high
+		jsonData = json.loads(projectGo.get_obligation(5))
+		self.assertEqual(jsonData['obligationid'],5)
+		#Typical case
+		jsonData = json.loads(projectGo.get_obligation(3))
+		self.assertEqual(jsonData['obligationid'],3)
+		#Checking other fields
+		jsonData = json.loads(projectGo.get_obligation(1))
+		self.assertEqual(jsonData['userid'],1)
+		jsonData = json.loads(projectGo.get_obligation(1))
+		self.assertEqual(jsonData['name'],"Dude name")
+		jsonData = json.loads(projectGo.get_obligation(1))
+		self.assertEqual(jsonData['starttime'],"2014-01-01 00:00:00.000")
+		jsonData = json.loads(projectGo.get_obligation(1))
+		self.assertEqual(jsonData['priority'],1)
 
 	#test of direct method for get_obligation with entry that doesn't exist
 	def test_go_get_obligation_not_exists(self):
-		result = projectGo.get_obligation(9999999)
-		jsonData = json.loads(result)
+		#Extreme case
+		jsonData = json.loads(projectGo.get_obligation(9999999))
+		self.assertEqual(jsonData['error'],1)
+		#Boundry case low
+		jsonData = json.loads(projectGo.get_obligation(0))
+		self.assertEqual(jsonData['error'],1)
+		#Boundry case high
+		jsonData = json.loads(projectGo.get_obligation(6))
 		self.assertEqual(jsonData['error'],1)
 		
 	def test_go_get_obligation_bad_key(self):
-		result = projectGo.get_obligation("this is supposed to be an int")
-		jsonData = json.loads(result)
+		#Invalid case string
+		jsonData = json.loads(projectGo.get_obligation("this is supposed to be an int"))
+		self.assertEqual(jsonData['error'],1)
+		#Invalid case float
+		jsonData = json.loads(projectGo.get_obligation(1.0))
+		self.assertEqual(jsonData['error'],1)
+		#Invalid case char
+		jsonData = json.loads(projectGo.get_obligation('a'))
 		self.assertEqual(jsonData['error'],1)
 
 	#test of GET method for get_obligation
 	def test_get_obligation(self):
+		#Edge case low
 		result = self.app.get('/obligations/1')
 		self.assertEquals(result.status, '200 OK')
+		#Edge case high
+		result = self.app.get('/obligations/5')
+		self.assertEquals(result.status, '200 OK')
+		#Typical case
+		result = self.app.get('/obligations/3')
+		self.assertEquals(result.status, '200 OK')
+
 	
 	#test of GET method for get_all_obligations
 	def test_get_all_obligations(self):
