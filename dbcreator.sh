8cf9c57ce6dd84d4df1b9e1b6b11993463d13f9c
Richard Friesen <frezen88@gmail.com>
2014-02-27 23:07:24 -0600

    new version of dbcreator
    using integer primary key for autoinc property
    added foreign keys

--- dbcreator.sh
+++ dbcreator.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
-mkdir Databases
-chmod 777 Databases
-chown user1 Databases
-cd Databases
+mkdir databases
+chmod 777 databases
+chown user1 databases
+DB=./databases/GoDB.db
+sqlite3 $DB "create table users(userid integer primary key not null, userName varchar(15));"
+sqlite3 $DB "create table userinfo(userid int primary key not null, name varchar(50), address varchar(50), birthday text, foreign key(userid) references users(userid));"
+sqlite3 $DB "create table obligation(obligationid integer primary key not null, userid int not null, name varchar(30), description varchar(200), starttime text, endtime text, priority int, status int, category int, foreign key(userid) references users(userid));"
+sqlite3 $DB "create table subObligation(sobligationid integer primary key not null, obligationid not null, name varchar(15), description varchar(200), starttime text, endtime text, priority int, status int, foreign key(obligationid) references obligation(obligationid));"
+sqlite3 $DB "create table reminder(reminderid integer primary key not null, obligationid int not null, remindertime text, description varchar(200), foreign key(obligationid) references obligation(obligationid));"
+sqlite3 $DB "create table alarm(alarmid integer primary key not null, obligationid int not null, alarmtime text, soundType int, foreign key(obligationid) references obligation(obligationid));"
+sqlite3 $DB "create table contactlist(userid int primary key not null, userlist text, foreign key(userid) references users(userid));"
 
-sqlite3 GoDB.db "create table users(userid int primary key not null, userName varchar(15));"
-sqlite3 GoDB.db "create table userinfo(userid int primary key not null, name varchar(50), address varchar(50), birthday text);"
-sqlite3 GoDB.db "create table obligation(obligationid int not null, userid int not null, name varchar(30), description varchar(200), starttime text, endtime text, priority int, status int, category int, primary key(userid, obligationid));"
-sqlite3 GoDB.db "create table subObligation(sobligationid int not null, obligationid not null, name varchar(15), description varchar(200), starttime text, endtime text, priority int, status int, primary key(sobligationid, obligationid));"
-sqlite3 GoDB.db "create table reminder(reminderid int not null, obligationid int not null, remindertime text, description varchar(200), primary key(reminderid, obligationid));"
-sqlite3 GoDB.db "create table alarm(alarmid int not null, obligationid int not null, alarmtime text, soundType int, primary key(alarmid, obligationid));"
-sqlite3 GoDB.db "create table contactlist(userid int primary key not null, userlist text);"
-
-chmod 777 GoDB.db
-cd ..
-
-echo "Attempted creating GoDB.db file (doesn't actually check yet, just asserts this script has finished)"
+echo "Attempted creating $DB file (doesn't actually check yet, just asserts this script has finished)"
